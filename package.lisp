8687a46821ef191f10f319805c78b7bb0c8ce441
attila.lendvai <attila.lendvai@gmail.com>
2008-06-04 23:06:18 +0000

    libgvc.so.4

--- package.lisp
+++ package.lisp
@@ -20,7 +20,7 @@
 (defvar *graphviz-foreign-library-directories* (list "/usr/lib/graphviz/" "/usr/lib/"))
 
 (define-foreign-library libgvc
-  (:unix (:or "libgvc.so" "libgvc32.so"))
+  (:unix (:or "libgvc.so.4" "libgvc32.so.4"))
   (:darwin "libgvc.so")
   (:windows ("libgvc.dll" "msvcrt.dll"))
   (t "libgvc"))
