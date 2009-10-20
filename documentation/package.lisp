ee68f8b1bd74268a6ed646e86c76a275e7aca826
levente.meszaros <levente.meszaros@gmail.com>
2009-10-20 14:09:34 +0000

    Follow hu.dwim.common-lisp renaming.

--- documentation/package.lisp
+++ documentation/package.lisp
@@ -8,7 +8,7 @@
 
 (defpackage :hu.dwim.graphviz.documentation
   (:use :hu.dwim.asdf
-        :hu.dwim.common-lisp
+        :hu.dwim.common
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.graphviz
--- test/package.lisp
+++ test/package.lisp
@@ -7,7 +7,7 @@
 (in-package :common-lisp-user)
 
 (defpackage :hu.dwim.graphviz.test
-  (:use :hu.dwim.common-lisp
+  (:use :hu.dwim.common
         :hu.dwim.def
         :hu.dwim.graphviz
         :hu.dwim.stefil
